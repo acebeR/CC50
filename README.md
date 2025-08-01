98d64c6aa5af797b84974bcd78f4bb3625169c0d
Rebeca <rebecadivinapc@gmail.com>
2025-08-01 15:52:38 -0300

    logica do exercicio de imagem

--- README.md
+++ README.md
@@ -3,7 +3,8 @@ CC50 - O Curso de Ciência da Computação de Harvard no Brasil
 
 # Exercícios Bacanas
 # Exercício aula 4 de memória
-- Aumentar o volume de um arquivo .war
+- Editar cor, blur... da imagem
+<img src="https://github.com/acebeR/CC50/blob/main/aula_04/filtro/edicao_de_imagem.gif?raw=true" style="width: 300px; height: 200px; object-fit: contain;" />
 # Exercício aula 0 de Scratch
 - Princesa
 link para jogar: https://scratch.mit.edu/projects/1194724247
--- aula_04/filtro/helpers.c
+++ aula_04/filtro/helpers.c
@@ -1,13 +1,112 @@
-#include "bmp.h"
+// Para rodar: make helpers
+// ./filter -g images/yard.bmp outgray.bmp     # Grayscale
+// ./filter -s images/yard.bmp outsepia.bmp     # Sepia
+// ./filter -r images/yard.bmp outReflect.bmp     # Reflect
+// ./filter -b images/yard.bmp outBlur.bmp     # Blur
+//check50 cs50/problems/2024/x/filter/less
 
+#include "helpers.h"
+#include <math.h>
 // Convert image to grayscale
-void grayscale(int height, int width, RGBTRIPLE image[height][width]);
+void grayscale(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int red = image[i][j].rgbtRed;
+            int green = image[i][j].rgbtGreen;
+            int blue = image[i][j].rgbtBlue;
 
+            int gray = round((red + green + blue) / 3.0);
+
+            image[i][j].rgbtRed = gray;
+            image[i][j].rgbtGreen = gray;
+            image[i][j].rgbtBlue = gray;
+        }
+    }
+}
 // Convert image to sepia
-void sepia(int height, int width, RGBTRIPLE image[height][width]);
+void sepia(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int originalRed = image[i][j].rgbtRed;
+            int originalGreen = image[i][j].rgbtGreen;
+            int originalBlue = image[i][j].rgbtBlue;
+
+            int sepiaRed = round(0.393 * originalRed + 0.769 * originalGreen + 0.189 * originalBlue);
+            int sepiaGreen = round(0.349 * originalRed + 0.686 * originalGreen + 0.168 * originalBlue);
+            int sepiaBlue = round(0.272 * originalRed + 0.534 * originalGreen + 0.131 * originalBlue);
+
+            image[i][j].rgbtRed = sepiaRed > 255 ? 255 : sepiaRed;
+            image[i][j].rgbtGreen = sepiaGreen > 255 ? 255 : sepiaGreen;
+            image[i][j].rgbtBlue = sepiaBlue > 255 ? 255 : sepiaBlue;
+        }
+    }
+}
 
 // Reflect image horizontally
-void reflect(int height, int width, RGBTRIPLE image[height][width]);
+void reflect(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width / 2; j++)
+        {
+            RGBTRIPLE temp = image[i][j];
+            image[i][j] = image[i][width - 1 - j];
+            image[i][width - 1 - j] = temp;
+        }
+    }
+}
+
 
 // Blur image
-void blur(int height, int width, RGBTRIPLE image[height][width]);
+void blur(int height, int width, RGBTRIPLE image[height][width])
+{
+    RGBTRIPLE copy[height][width];
+
+    // Copiar imagem original
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            copy[i][j] = image[i][j];
+        }
+    }
+
+    // Aplicar blur
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int redSum = 0, greenSum = 0, blueSum = 0;
+            int count = 0;
+
+            // Iterar pelos vizinhos
+            for (int di = -1; di <= 1; di++)
+            {
+                for (int dj = -1; dj <= 1; dj++)
+                {
+                    int ni = i + di;
+                    int nj = j + dj;
+
+                    // Verifica se está dentro dos limites da imagem
+                    if (ni >= 0 && ni < height && nj >= 0 && nj < width)
+                    {
+                        redSum += copy[ni][nj].rgbtRed;
+                        greenSum += copy[ni][nj].rgbtGreen;
+                        blueSum += copy[ni][nj].rgbtBlue;
+                        count++;
+                    }
+                }
+            }
+
+            image[i][j].rgbtRed = round((float) redSum / count);
+            image[i][j].rgbtGreen = round((float) greenSum / count);
+            image[i][j].rgbtBlue = round((float) blueSum / count);
+        }
+    }
+}
